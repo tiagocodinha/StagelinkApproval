a9c1693559edff48416bce28fa6112043c82db71
tiagocodinha <98787370+tiagocodinha@users.noreply.github.com>
2025-02-17 21:28:12 +0000

    Commit from Bolt to GitHub

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,5 +1,6 @@
-// Initialize Supabase client
+import { createClient } from '@supabase/supabase-js';
+
 const supabaseUrl = 'https://ctyqetwzowcqqxcwiukd.supabase.co';
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN0eXFldHd6b3djcXF4Y3dpdWtkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzk4MjA2NzksImV4cCI6MjA1NTM5NjY3OX0.V1Fd4siW6lP5HnfWK-evSae_CXk7pAX0luZZnbcCe3o';
 
-export const supabase = supabase.createClient(supabaseUrl, supabaseKey);
+export const supabase = createClient(supabaseUrl, supabaseKey);
